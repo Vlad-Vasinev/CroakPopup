13efb073b2b1fc1f821b218100e5df3dce7a42ff
Vladislav Vasinev <80538465+Vlad-Vasinev@users.noreply.github.com>
2025-03-01 16:16:39 +0300

    Add files via upload

--- js/functions/storiesOut.js
+++ js/functions/storiesOut.js
@@ -1,28 +1,8 @@
 
-import { delStoriesContainer } from "./delStoriesContainer.js"
-import { enableScroll } from "./scrollFunc.js"
+import { closeStories } from "./closeStories.js"
 
 export function storiesExit (storiesOut, startXSwipe, startYSwipe, storiesContainer, storiesGalleri, keyEvent, mobTouchStart, mobTouchEnd) {
   storiesOut.addEventListener('click', () => {
-    document.removeEventListener('keydown', keyEvent)
-    startXSwipe = 0
-    startYSwipe = 0
-    document.removeEventListener('touchstart', mobTouchStart)
-    document.removeEventListener('touchend', mobTouchEnd)
-    window.countIndex = undefined
-    delStoriesContainer(storiesContainer)
-    enableScroll()
-    storiesGalleri.classList.remove('galleri_transform')
-
-    if(window.buttons) {
-      setTimeout(() => {
-
-        storiesGalleri.parentElement.parentElement.querySelector('.stories-prev').removeAttribute('disabled')
-        storiesGalleri.parentElement.parentElement.querySelector('.stories-prev').classList.remove('btn_disabled')
-
-        storiesGalleri.parentElement.parentElement.querySelector('.stories-next').removeAttribute('disabled')
-        storiesGalleri.parentElement.parentElement.querySelector('.stories-next').classList.remove('btn_disabled')
-      }, 300)
-    }
+    closeStories(startXSwipe, startYSwipe, storiesContainer, storiesGalleri, keyEvent, mobTouchStart, mobTouchEnd)
   })
 }
