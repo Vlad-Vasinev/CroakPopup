ef7f29bd938d8e53d966ed90fb4496b492da39f7
Vlad-Vasinev <vda5petr@rambler.ru>
2025-04-25 23:57:45 +0300

    will-change transform

--- js/croak.js
+++ js/croak.js
@@ -204,7 +204,7 @@ let frog = new croakSlider({
   //mobileVideo: true,
   deskStories: true,
   deskSwipe: true,
-  deskSwipeFocus: true,
+  //deskSwipeFocus: true,
   keyboard: true,
   //buttons: true,
 });
